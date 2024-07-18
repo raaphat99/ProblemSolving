258001f84a55f9f8df0653e5c354af070d5e2b91
Ahmed Raaphat <60491788+raaphat99@users.noreply.github.com>
2024-07-18 14:30:14 +0300

    Create EvenOdds.cpp
    Problem Link: https://codeforces.com/contest/318/problem/A

---
+++ Codeforces/Even Odds/EvenOdds.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+long long getResult(long long n, long long k, long long mid)
+{
+    long long out;
+    if (k <= mid)
+        // first half
+        out = (2 * (k-1)) + 1;
+    else
+        // second half
+        out = 2 * (k - mid);
+    return out;
+}
+
+int main()
+{
+    long long n, k, out, middle;
+    cin >> n >> k;
+    if (n % 2 == 0)
+    {
+        // even
+        middle = n/2;
+        cout << getResult(n, k, middle);
+    }
+    else
+    {
+        // odd
+        middle = (n/2)+1;
+        cout << getResult(n, k, middle);
+    }
+    return 0;
+}
