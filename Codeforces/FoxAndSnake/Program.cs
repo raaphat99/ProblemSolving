b4690ef61f2b81dd31fa2008432cedfb13c36d10
Ahmed Raaphat <60491788+raaphat99@users.noreply.github.com>
2024-11-18 19:53:16 +0200

    Add files via upload
    Problem Link: https://codeforces.com/contest/510/problem/A

---
+++ Codeforces/FoxAndSnake/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxAndSnake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+            int x = numbers[0], y = numbers[1];
+            string flag = "dot"; // "hash"
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = 1; j <= y; j++)
+                {
+                    if (i % 2 != 0 || (flag == "dot" && j == y) || (flag != "dot" && j == 1))
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                if (i % 2 == 0)
+                    flag = flag == "dot" ? "hash" : "dot";
+
+                Console.WriteLine();
+            }
+
+            //for (int i = 1; i <= x; i++)
+            //{
+            //    for (int j = 1; j <= y; j++)
+            //    {
+            //        if (i % 2 != 0)
+            //        {
+            //            Console.Write("#");
+            //        }
+            //        else
+            //        {
+            //            if (flag == "dot")
+            //            {
+            //                if (j == y)
+            //                    Console.Write("#");
+            //                else
+            //                    Console.Write(".");
+            //            }
+            //            else
+            //            {
+            //                if (j == 1)
+            //                    Console.Write("#");
+            //                else
+            //                    Console.Write(".");
+            //            }
+            //        }
+            //    }
+            //    if (i % 2 == 0)
+            //        flag = flag == "dot" ? "hash" : "dot";
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
